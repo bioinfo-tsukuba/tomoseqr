e5d2e3fbe4f63846e48d14b302aa4e368571ec43
shingenmochi <kinakomochi.work@gmail.com>
2022-01-19 21:52:40 +0900

    add auto slider

--- R/ImageViewer.R
+++ R/ImageViewer.R
@@ -8,14 +8,21 @@ ImageViewer <- function (tomoObj, geneID) {
                     type="tabs",
                     tabPanel(
                         "x",
+                        plotOutput("xplot"),
                         sliderInput(
                             "xSec",
                             "Section",
                             min=1,
                             max=dim(recResult)[1],
                             step=1,
-                            value=1
+                            value=1,
+                            # animate=TRUE,
+                        animate = animationOptions(
+                            interval=70,
+                            loop=TRUE
                         )
+                        ),
+                        downloadButton("downloadFig", label = "Download figure")
                     ),
                     tabPanel(
                         "y",
@@ -43,6 +50,19 @@ ImageViewer <- function (tomoObj, geneID) {
             )
         )
     }
-    server <- function (input, output) {}
+    server <- function (input, output) {
+        output$xplot <- renderPlot({
+            filled.contour(recResult[,,input$xSec], zlim=c(min(recResult), max(recResult)))
+            # plot(1, input$xSec)
+        })
+        output$downloadFig <- downloadHandler(
+            filename = "xPlot.png",
+            content = function (file) {
+                png(file, height=1000, width=1000, res=144)
+                filled.contour(recResult[,,input$xSec], zlim=c(min(recResult), max(recResult)))
+                dev.off()
+            }
+        )
+    }
     shinyApp(ui, server)
 }
