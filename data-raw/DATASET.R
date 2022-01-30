ad17265b95ac6719248f78f83d67cd9417eb030c
shingenmochi <kinakomochi.work@gmail.com>
2022-01-31 08:23:22 +0900

    change testData

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,43 +1,71 @@
-
-## code to prepare `DATASET` dataset goes here
-test_data <- array(0, dim = c(50, 50, 50))
-for (x in 1:50){
-    for (y in 1:50) {
-        for (z in 1:50) {
-             if (sqrt((x-10.1)^2+(y-35.1)^2 + (z - 35.1)^2) <= 7.5) {
-                test_data[x, y, z] <- (1000 / sqrt((x-10.1)^2+(y-35.1)^2 + (z - 35.1)^2))
-             }
+rm(list = ls())
+library(tidyverse)
+generateTomoseqData <- function(
+    xLen,
+    yLen,
+    zLen,
+    func,
+    tValue = 1,
+    fValue = 0
+) {
+    mask <- array(0, dim = c(xLen, yLen, zLen))
+    indexList <- expand.grid(1:xLen, 1:yLen, 1:zLen)
+    for (i in seq_len(length(indexList[,1]))) {
+        x <- indexList[i, 1]
+        y <- indexList[i, 2]
+        z <- indexList[i, 3]
+        if (func(x, y, z) == TRUE) {
+            mask[x, y, z] <- tValue
+        } else {
+            mask[x, y, z] <- fValue
         }
     }
-}
-tomoseq_test_x <- apply(test_data, 1, sum)
-tomoseq_test_y <- apply(test_data, 2, sum)
-tomoseq_test_z <- apply(test_data, 3, sum)
-tomoseq_test_x <- t(tomoseq_test_x) %>% as.data.frame()
-tomoseq_test_y <- t(tomoseq_test_y) %>% as.data.frame()
-tomoseq_test_z <- t(tomoseq_test_z) %>% as.data.frame()
-
-add_to_data <- function (data) {
-    add_data <- runif(50) * 1000
-    return(rbind(data, add_data))
+    return(mask)
 }
 
-for (i in 1:49) {
-    tomoseq_test_x <- add_to_data(tomoseq_test_x)
-    tomoseq_test_y <- add_to_data(tomoseq_test_y)
-    tomoseq_test_z <- add_to_data(tomoseq_test_z)
+maskFunc <- function(x, y, z) {
+    return(
+        (x - 25)^2 + 5 * (y - 25)^2 + 10 * (z - 25)^2 <= 25^2
+    )
 }
-
-row_name <- c()
-for (i in 1:50) {
-    row_name <- append(row_name, paste("gene", i, sep=""))
+func1 <- function(x, y, z) {
+    return(
+        10 * (x - 25)^2 + 50 * (y - 25)^2 + 100 * (z - 25)^2 <= 25^2
+    )
 }
-col_name <- as.character(c("gene_ID", seq(1,50)))
-tomoseq_test_x <- cbind(row_name, tomoseq_test_x)
-tomoseq_test_y <- cbind(row_name, tomoseq_test_y)
-tomoseq_test_z <- cbind(row_name, tomoseq_test_z)
+func3 <- function(x, y, z) {
+    return(
+        (x - 12.5)^2 + (y - 25)^2 + (z - 25)^2 <= (25 / 16)^2
+    )
+}
+exp1 <- generateTomoseqData(50, 50, 50, func1, tValue = 100, fValue = 0)
+expTmp <- generateTomoseqData(50, 50, 50, maskFunc, tValue = 10)
+exp2 <- exp1 + expTmp
+exp2[exp2 == 110] <- 100
+exp3 <- generateTomoseqData(50, 50, 50, func3, tValue = 100, fValue = 0)
+mask <- generateTomoseqData(50, 50, 50, maskFunc)
 
-colnames(tomoseq_test_x) <- col_name
-colnames(tomoseq_test_y) <- col_name
-colnames(tomoseq_test_z) <- col_name
-usethis::use_data(tomoseq_test_x, tomoseq_test_y, tomoseq_test_z, overwrite = TRUE)
+x1 <- apply(exp1, 1, sum)
+x2 <- apply(exp2, 1, sum)
+x3 <- apply(exp3, 1, sum)
+x4 <- 30000 - (x1 + x2 + x3)
+y1 <- apply(exp1, 2, sum)
+y2 <- apply(exp2, 2, sum)
+y3 <- apply(exp3, 2, sum)
+y4 <- 30000 - (y1 + y2 + y3)
+z1 <- apply(exp1, 3, sum)
+z2 <- apply(exp2, 3, sum)
+z3 <- apply(exp3, 3, sum)
+z4 <- 30000 - (z1 + z2 + z3)
+
+testx <- rbind(x1, x2, x3, x4)
+testy <- rbind(y1, y2, y3, y4)
+testz <- rbind(z1, z2, z3, z4)
+rownames(testx) <- c("gene1", "gene2", "gene3", "gene4")
+rownames(testy) <- c("gene1", "gene2", "gene3", "gene4")
+rownames(testz) <- c("gene1", "gene2", "gene3", "gene4")
+testx <- testx %>% as_tibble(rownames = "geneID")
+testy <- testy %>% as_tibble(rownames = "geneID")
+testz <- testz %>% as_tibble(rownames = "geneID")
+
+usethis::use_data(testx, testy, testz, mask, overwrite = TRUE)
