093d2aa1e110385d4304c7fec84e06851cb6528b
shingenmochi <kinakomochi.work@gmail.com>
2022-01-31 09:14:11 +0900

    change test

--- tests/testthat/test-Estimate3dExpressions.R
+++ tests/testthat/test-Estimate3dExpressions.R
@@ -1,6 +1,5 @@
 test_that("Reconstruction of 3D expression pattern", {
-    load("simulateData.rda")
-    load("rec_results.rda")
+    load("trueResults.rda")
     tomoSeqObject <- Estimate3dExpressions(
         testx,
         testy,
@@ -12,8 +11,13 @@ test_that("Reconstruction of 3D expression pattern", {
     result2 <- GetReconstructedResult(tomoSeqObject, "gene2")
     result3 <- GetReconstructedResult(tomoSeqObject, "gene3")
     result4 <- GetReconstructedResult(tomoSeqObject, "gene4")
-    expect_identical(sum((result1 - gene1_rec)^2), 0)
-    expect_identical(sum((result2 - gene2_rec)^2), 0)
-    expect_identical(sum((result3 - gene3_rec)^2), 0)
-    expect_identical(sum((result4 - gene4_rec)^2), 0)
+
+    true1 <- GetReconstructedResult(trueResults, "gene1")
+    true2 <- GetReconstructedResult(trueResults, "gene2")
+    true3 <- GetReconstructedResult(trueResults, "gene3")
+    true4 <- GetReconstructedResult(trueResults, "gene4")
+    expect_identical(sum((result1 - true1)^2), 0)
+    expect_identical(sum((result2 - true2)^2), 0)
+    expect_identical(sum((result3 - true3)^2), 0)
+    expect_identical(sum((result4 - true4)^2), 0)
 })
